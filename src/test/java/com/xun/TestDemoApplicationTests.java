841cb29d3d5c8f1eb29329a0e0c9e2a819d52b9a
guizy <coderzygui@gmail.com>
2026-04-01 15:28:56 +0800

    feat: init project

---
+++ src/test/java/com/xun/TestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
